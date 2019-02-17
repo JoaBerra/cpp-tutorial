53d8f440319f3dd5c2a39bdc5c16d2d716aa3a69
JoaBerra <joakim.bergelin@gmail.com>
2019-02-17 08:16:15 +0100

    Just trying to get to fresh starting point

--- main.cpp
+++ main.cpp
@@ -1,24 +1,85 @@
-// operating with variables
+a// operating with variables
 
 #include <iostream>
-#include <string>
 using namespace std;
 
+void odd(int a);
+void even(int a);
+int billy[] = {16, 2, 77, 40, 12071};
+int n, result = 0;
+
+int subtraction(int a, int b){
+  int r;
+  r = a - b;
+  return (r);
+}
+
+void duplicate(int& a, int& b, int& c){
+  a *= 2;
+  b *= 2;
+  c *= 2;
+}
+
+void printarray(int arg[], int length){
+  for (int n = 0; n < length; n++){
+    cout << arg[n] << " ";
+  }
+  cout << "\n";
+}
+
+void prevnext(int x, int& prev, int& next){
+  prev = x - 1;
+  next = x + 1;
+}
+
+int divide(int a, int b = 2){
+  int r;
+  r = a / b;
+  return (r);
+}
+
+int operate(int a, int b){
+  return (a * b);
+}
+
+float operate(float a, float b){
+  return(a / b);
+}
+
+long factorial(long a){
+  if(a > 1)
+    return(a * factorial(a -1));
+  else
+    return(1);
+}
+
 int main(int argc, char const *argv[]) {
-  // declaring variables:
-  string mystr;
+  int firstvalue = 5, secondvalue = 15;
+  int * p1, * p2;
 
-  cout << "What is your name: ";
-  getline(cin, mystr);
+  p1 = &firstvalue;
+  p2 = &secondvalue;
+  *p1 = 10;
+  *p2 = *p1;
+  p1 = p2;
+  *p1 = 20;
 
-  // print out the result
-  cout << "Hello " << mystr << ".\n";
-  cout << "What is your favorite team? ";
-  getline(cin, mystr)
-  cout << "I like " << mystr << " too!\n";
+  cout << "firstvalue is " << firstvalue << endl;
+  cout << "secondvalue is " << secondvalue << endl;
 
-  // test getline function
-
-  // terminate the program:
   return 0;
+}
+
+void odd(int a){
+  if((a%2) != 0)
+    cout << "Number is odd.\n";
+  else
+    even(a);
+}
+
+void even(int a){
+  if((a % 2) == 0)
+    cout << "Number is even.\n";
+  else
+    odd(a);
 }
